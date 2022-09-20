b2565fd164a454d230d35abd101b12f6d1de151e
sizwe <sizwemorrismnothoza50@gmail.com>
2022-09-20 16:22:56 +0200

    Project starting files

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import fetch from 'node-fetch';
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors())
+
+const PORT = process.env.PORT || 3001
+
+app.post('/search', async (req, res) => {
+    let term = req.body.term;
+    let mediaType = req.body.mediaType;
+
+    if (mediaType == "all") {
+        const response = await fetch(`https://itunes.apple.com/search?term=${term}`)
+        const data = await response.json().catch((e) => {
+            console.log(e)
+            res.json(e);
+        });
+        res.json(data);
+    } else {
+        let response = await fetch(`https://itunes.apple.com/search?term=${term}&entity=${mediaType}`);
+        const data = await response.json().catch((e) => {
+            console.log(e);
+            res.json(e);
+        });
+        res.json(data);
+    };
+});
